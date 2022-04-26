6395c8f39e06474aa1b76d83681e63cf87759d77
Yamini <ybansal@spendlabs.com>
2022-04-26 15:47:12 +0530

    Improved dependency Injection with ZLayer

--- src/main/scala/WordCounter.scala
+++ src/main/scala/WordCounter.scala
@@ -1,35 +1,29 @@
+import zio._
+
 import java.io.IOException
-import scala.io.{BufferedSource, Source}
 
-object WordCounter extends zio.App{
+object WordCounter  {
+  type WordCounterEnv = Has[WordCounter.Service]
 
-  import zio._
-  import zio.console._
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = (for {
-    fiber1 <- convert("https://zio.dev/version-1.x/overview/overview_handling_resources").fork
-    fiber2 <- convert("https://zio.dev/version-1.x/overview/overview_basic_concurrency").fork
-    countForFiber1 <-  fiber1.join
-    countForFiber2 <- fiber2.join
-    _ <- putStrLn(countForFiber1)
-    _ <- putStrLn(countForFiber2)
-  } yield ()).exitCode
-
-  def readFile(address: String): BufferedSource = {
-    Source.fromURL(address)
-  }
-
-  def open(address: String): Managed[IOException, Source] = {
-    val acquire: ZIO[Any, IOException, BufferedSource]  = ZIO(readFile(address)).refineToOrDie[IOException]
-    val release: Source => URIO[Any, Unit] = (source: Source) => ZIO(source.close()).orDie
-    Managed.make(acquire)(release)
-  }
-
-  def convert(address: String): ZIO[Any, IOException, String] = {
-    open(address).use {
-      source => ZIO(source.getLines.flatMap(_.split("\\W+"))
-        .foldLeft(Map.empty[String, Int]){
-          (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
-        }.foldLeft(0)(_+_._2).toString).refineToOrDie[IOException]
+  class Service(reader: SourceReader.Service) {
+    def convert(address: List[String]): Task[String] = {
+      ZIO.foreach(address) { addresses =>
+        reader.open(addresses).use {
+          source => ZIO(source.getLines.flatMap(_.split("\\W+"))
+            .foldLeft(Map.empty[String, Int]){
+              (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
+            }).refineToOrDie[IOException]
+        }
+      }.map { listOfCountsWithWords =>
+        listOfCountsWithWords.flatten.groupBy(_._1).mapValues(_.map(_._2).sum).mkString("\n")
+      }.refineToOrDie[IOException]
     }
   }
+
+  val live: ZLayer[Has[SourceReader.Service], Nothing, WordCounterEnv] = ZLayer.fromService[SourceReader.Service, WordCounter.Service]{ sourceReader =>
+    new Service(sourceReader)
+  }
+
+  def convert(address: List[String]): ZIO[WordCounterEnv, Throwable, String] =
+    ZIO.accessM(_.get.convert(address))
 }
